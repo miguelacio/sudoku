a652d61ceae2de6a73289eed7b1afb6a11b26722
Jesus Miguel Flores <miguelacio@MacBook-Air-de-Jesus.local>
2018-01-12 14:39:34 -0700

    segundo commmit con clases documentadas

--- app/src/main/java/mx/miguelacio/sudokuandroid/models/Board.java
+++ app/src/main/java/mx/miguelacio/sudokuandroid/models/Board.java
@@ -6,17 +6,20 @@ import java.util.ArrayList;
  */
 
 public class Board {
+    //modelo de datos Board
 
     private int[][] gameCells = new int[9][9];
+    //Matriz 9x9 porque asi de grande es un sudoku
 
     public Board() {
-
+    //constructor vacio
     }
-
+    //En este método asignamos el valor obtenido en el lugar que queremos
     public void setValue(int row, int column, int value) {
         gameCells[row][column] = value;
     }
 
+    //Aquí copiamos la matriz
     public void copyValues(int[][] newGameCells) {
         for (int i = 0; i < newGameCells.length; i++) {
             for (int j = 0; j < newGameCells[i].length; j++) {
@@ -24,10 +27,12 @@ public class Board {
             }
         }
     }
+    //un simple get
     public int[][] getGameCells() {
         return gameCells;
     }
 
+    //este metodo nos regresa el valor al darle la columna y fila
     public int getValue(int row, int column) {
         return gameCells[row][column];
     }
